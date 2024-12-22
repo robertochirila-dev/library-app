33bb93941f7b6ffff237f9b96ddbbf05f0b79bed
robertochirila-dev <robertochirila@Robertos-MacBook-Pro.local>
2024-12-22 17:09:40 +0200

    added the ReaderList component and added ability to toggle between books and readers

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,34 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <BookList></BookList>
+  <div>
+    <div class="flex justify-between mb-4">
+      <button @click="showBooks = true" class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+        Show Books
+      </button>
+      <button @click="showBooks = false" class="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+        Show Readers
+      </button>
+    </div>
+    <BookList v-if="showBooks"></BookList>
+    <ReaderList v-else></ReaderList>
+  </div>
 </template>
 
 <script>
 import BookList from './components/BookList.vue'
+import ReaderList from './components/ReaderList.vue'
 
 
 export default {
   name: 'App',
   components: {
-    BookList
+    BookList,
+    ReaderList
+  },
+  data() {
+    return {
+      showBooks: false,
+    }
   }
 }
 </script>
---
+++ src/components/ReaderList.vue
@@ -0,0 +1,28 @@
+<template>
+    <p>Readers</p>
+</template>
+
+<script>
+export default {
+    name: 'ReaderList',
+    data () {
+      return {
+        readers: []
+      }
+    },
+    mounted() {
+    this.fetchReaders();
+  },
+    methods: {
+        async fetchReaders() {
+            const response = await fetch('http://localhost:3000/readers');
+            const data = await response.json();
+            this.readers = data;
+            return ''
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
